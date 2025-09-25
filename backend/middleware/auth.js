473935a2953be660b5af87577ad63cc9b7f2df62
vrlegacy <vrlegacy.care@gmail.com>
2025-09-25 23:22:06 +0530

    some additions

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,7 @@
 // backend/middleware/auth.js
+//this file contain logic of login and tokens ,
+// if credentiols are correct then it replace heade with actaul token and sent that to server
+//else it sends error message
 const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
@@ -8,13 +11,14 @@ function auth(req, res, next) {
   const token = req.header("Authorization")?.replace("Bearer ", "");
   if (!token)
     return res.status(401).json({ message: "No token, authorization denied" });
-
+  // if token is not found , this message will returned
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
+    // if token is wrong, this message will returned.
   }
 }
 
