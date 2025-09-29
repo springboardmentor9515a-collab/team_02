21ed0d474f57c4c90db7fa2f67868ded7f61590e
KomalSWaghule <komalwaghule05@gmail.com>
2025-09-29 18:46:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const mongo=require("mongoose")
 const bcrypt=require("bcrypt")
 const jwt =require("jsonwebtoken")
 const express=require("express")
-const {signin,signup}= require("./backend/auth")
+const {signin,signup,user}= require("./auth")
 const bodyParser = require("body-parser")
 require("dotenv").config();
 const path = require("path");
@@ -10,7 +10,8 @@ const cors = require("cors");
 const route =express();
 route.use(bodyParser.json());
 route.use(cors());
-
+const verifyUser = require("./middleware/authmiddleware");
+const { error } = require("console")
 mongo.connect(process.env.mongourl).then(()=> console.log("mongodb connected")).
     catch(e=>console.error(e));
 
@@ -29,6 +30,10 @@ route.post("/signup",async(req ,res )=>{
 
 route.post("/signin",async(req ,res )=>{
   try {  const signedin = await signin(req.body);
+
+    if (signedin.error){
+      return res.status(400).json({error:signedin.error})
+    }
     res.json(signedin);
   } catch (err) {
     console.error("Signin error:", err); 
@@ -36,5 +41,17 @@ route.post("/signin",async(req ,res )=>{
   }
 });
 
+
+route.get("/dashboard",verifyUser,async(req,res)=>{
+    try {
+      const currentuser= await user.findById(req.userid).select("-password");
+      if(!currentuser) return res.status(400).json({error:"user not found"});
+      res.json(currentuser);
+
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({error:"server error"});
+    }
+});
 const PORT = 5000;
 route.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
