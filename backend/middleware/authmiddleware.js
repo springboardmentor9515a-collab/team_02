b0e18344cdef212eb42de36441a81c40990e058e
KomalSWaghule <komalwaghule05@gmail.com>
2025-10-01 21:14:06 +0530

    Update authmiddleware.js

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -2,17 +2,18 @@ const jwt =require("jsonwebtoken");
 require("dotenv").config();
 const secret =process.env.jwtsecret;
 
-function verifyuser(req,res,next){
+async function verifyuser(req,res,next){
     const header= req.headers["authorization"];
     if(!header) throw new Error("No token received");
 
   const token =header.split(" ")[1];
+  const isBlacklisted = await Blacklist.findOne({ token });
+    if (isBlacklisted) return res.status(401).json({ message: "Token is invalidated" });
+
   jwt.verify(token,secret,(err,decode)=>{
         if (err) throw new Error("invalid token");
         req.userid =decode.id;
         next();
-
-
 
   });
 
