71a0982ed9bab1ca2d2465a283d9cb69aceb6f0c
KomalSWaghule <komalwaghule05@gmail.com>
2025-10-16 10:22:16 +0530

    Create verifyofficial.js

---
+++ backend/middleware/verifyofficial.js
@@ -0,0 +1,19 @@
+
+const {user:User} = require("../auth");
+
+async function verifyOfficial(req, res, next) {
+  try {
+    const currentUser = await User.findById(req.userid);
+    if (!currentUser) return res.status(404).json({ message: "User not found." });
+
+    if (currentUser.role !== "Official") {
+      return res.status(403).json({ message: "Access denied. Only officials can assign petitions." });
+    }
+
+    next();
+  } catch (err) {
+    res.status(500).json({ message: "Error verifying official", error: err.message });
+  }
+}
+
+module.exports = verifyOfficial;
