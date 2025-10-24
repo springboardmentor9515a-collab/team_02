30be3514cd31f0c96a4c8dfd7293a24429507561
KomalSWaghule <komalwaghule05@gmail.com>
2025-10-24 20:04:12 +0530

    Create vote.js

---
+++ backend/model/vote.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const VoteSchema = new mongoose.Schema({
+  poll_id: { type: mongoose.Schema.Types.ObjectId, ref: "Poll", required: true },
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  selected_option: { type: String, required: true }
+}, { timestamps: true });
+
+VoteSchema.index({ poll_id: 1, user_id: 1 }, { unique: true });
+
+module.exports = mongoose.model("Vote", VoteSchema);
